54de195e0479e986ef2b68f3b22d47f9a494926e
halim <haleemyoosuph@gmail.com>
2020-05-03 20:05:03 +0100

    extracted colorBox css styles to js styles using material styles

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { withStyles } from '@material-ui/styles';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import styles from '../styles/ColorBoxStyles';
 import chroma from 'chroma-js';
 import './ColorBox.css';
 import { Link } from 'react-router-dom';
 
-const ColorBoxes = ({ color: colorObj, colorFormat, paletteId }) => {
+const ColorBoxes = ({ color: colorObj, colorFormat, paletteId, classes }) => {
   const [copied, setCopied] = useState(false);
   const changeCopied = () => {
     setCopied(true);
@@ -16,21 +18,25 @@ const ColorBoxes = ({ color: colorObj, colorFormat, paletteId }) => {
   const lightness = luminance > 0.7;
   const colorClass = lightness ? 'black-text' : '';
   return (
-    <div style={{ backgroundColor: color }} className="color-box">
-      <div className="copy-container">
+    <div style={{ backgroundColor: color }} className={classes.colorBox}>
+      <div>
         <div
           style={{ background: color }}
-          className={`overlay ${copied && `show`}`}
+          className={`${classes.overlay} ${copied && `${classes.showOverlay}`}`}
         />
-        <div className={`copied-text ${copied && `show`} ${colorClass}`}>
+        <div
+          className={`${classes.copiedText} ${
+            copied && `${classes.showCopiedText}`
+          } ${colorClass}`}
+        >
           <h1>Copied!!</h1>
           <p>{color}</p>
         </div>
-        <div className={`box-content ${colorClass}`}>
-          <span>{name}</span>
+        <div className={classes.boxContent}>
+          <span className={classes.textColor}>{name}</span>
         </div>
         <CopyToClipboard text={name}>
-          <button onClick={changeCopied} className={`copy ${colorClass}`}>
+          <button onClick={changeCopied} className={classes.copyButton}>
             COPY
           </button>
         </CopyToClipboard>
@@ -38,7 +44,7 @@ const ColorBoxes = ({ color: colorObj, colorFormat, paletteId }) => {
       {paletteId && (
         <div className="more-section">
           <Link to={`/palette/${paletteId}/${id}`}>
-            <button className={`more ${colorClass}`}>MORE</button>
+            <button className={classes.more}>MORE</button>
           </Link>
         </div>
       )}
@@ -46,4 +52,4 @@ const ColorBoxes = ({ color: colorObj, colorFormat, paletteId }) => {
   );
 };
 
-export default ColorBoxes;
+export default withStyles(styles)(ColorBoxes);
---
+++ src/styles/ColorBoxStyles.js
@@ -0,0 +1,103 @@
+import chroma from 'chroma-js';
+export default {
+  textColor: {
+    color: (props) => {
+      const color = props.color[props.colorFormat];
+      return chroma(color).luminance() > 0.7 ? 'black' : 'white';
+    },
+  },
+  colorBox: {
+    width: '20%',
+    height: '25%',
+    display: 'inline-block',
+    cursor: 'pointer',
+    position: 'relative',
+    textTransform: 'uppercase',
+    '&:hover button': {
+      opacity: 1,
+    },
+  },
+  boxContent: {
+    position: 'absolute',
+    bottom: '0',
+    left: '10px',
+    color: 'white',
+  },
+  copyButton: {
+    color: (props) => {
+      const color = props.color[props.colorFormat];
+      return chroma(color).luminance() > 0.7 ? 'black' : 'white';
+    },
+    backgroundColor: 'rgb(221, 221, 221, 0.3)',
+    border: '0',
+    width: '100px',
+    height: '30px',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginLeft: '-50px',
+    marginTop: '-15px',
+    opacity: '0',
+    textAlign: 'center',
+    lineHeight: '30px',
+    cursor: 'pointer',
+    outlineColor: 'white',
+    textDecoration: 'none',
+  },
+  overlay: {
+    width: '100%',
+    height: '100%',
+    zIndex: '0',
+    opacity: '0',
+    transition: '0.5s',
+  },
+  showOverlay: {
+    opacity: '1',
+    transform: 'scale(10)',
+    zIndex: '10',
+    position: 'fixed',
+  },
+  copiedText: {
+    margin: 'auto',
+    position: 'fixed',
+    display: 'none',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    top: '0',
+    left: '0',
+    bottom: '0',
+    right: '0',
+    opacity: '0',
+    color: 'white',
+    fontWeight: '200',
+    fontFamily: 'fantasy',
+    width: '100%',
+    height: '100px',
+    backgroundColor: 'rgb(221, 221, 221, 0.3)',
+  },
+  showCopiedText: {
+    color: (props) => {
+      const color = props.color[props.colorFormat];
+      return chroma(color).luminance() > 0.7 ? 'black' : 'white';
+    },
+    opacity: '1',
+    zIndex: '20',
+    display: 'flex',
+  },
+  more: {
+    color: (props) => {
+      const color = props.color[props.colorFormat];
+      return chroma(color).luminance() > 0.7 ? 'black' : 'white';
+    },
+    position: 'absolute',
+    bottom: '0',
+    right: '0',
+    backgroundColor: 'rgb(221, 221, 221, 0.3)',
+    textAlign: 'center',
+    lineHeight: '30px',
+    border: 'none',
+    outlineColor: 'white',
+    cursor: 'pointer',
+  },
+};
